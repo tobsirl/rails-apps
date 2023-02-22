38c7f9a302ed57e33cf79730c7743fd91118f8db
Paul Tobin <tobster111@gmail.com>
2023-02-22 19:01:58 +0000

    Rails button route

--- button/app/controllers/pages_controller.rb
+++ button/app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @posts = Post.first
+    @post = Post.first if Post.any?
   end
 end
--- button/config/routes.rb
+++ button/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # Route for the upvote_post_path for the put method
+  # /posts/:id/upvote => posts#upvote
+  put 'posts/:id/upvote', to: 'posts#upvote', as: 'upvote_post'
   root 'pages#home'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
