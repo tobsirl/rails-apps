ba9eaf0578d41e52d5e35d1f983ea3568bb2e445
Paul Tobin <tobster111@gmail.com>
2023-02-22 18:56:29 +0000

    Rails button

--- button/config/routes.rb
+++ button/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   resources :posts
-  root 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
