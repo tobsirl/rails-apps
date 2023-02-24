0b415dc1f1fa0d615ee8eb111cf328b2d4455251
Paul Tobin <tobster111@gmail.com>
2023-02-24 18:45:40 +0000

    routes

--- routes/config/routes.rb
+++ routes/config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
   root "posts#index"
   # resources :posts
+
+  # All posts route
+  get "/posts", to: "posts#index"
+
+  # New post route
+
+  # Create post route
+
+  # Show post route
+
+  # Edit post route
+
+  # Update post route
+
+  # Delete post route
+
 end
