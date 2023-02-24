90d4cc955b2f746851e4cea279fe2298539e74b8
Paul Tobin <tobster111@gmail.com>
2023-02-24 18:56:23 +0000

    routes

--- routes/config/routes.rb
+++ routes/config/routes.rb
@@ -6,14 +6,19 @@ Rails.application.routes.draw do
   get "/posts", to: "posts#index"
 
   # New post route
+  get "/posts/new", to: "posts#new", as: "new_post"
 
   # Create post route
+  post "/posts", to: "posts#create"
 
   # Show post route
+  get "/posts/:id", to: "posts#show", as: "post"
 
   # Edit post route
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
 
   # Update post route
+  patch "/posts/:id", to: "posts#update"
 
   # Delete post route
 
