1577611be4b212f8efbff32668a57d6695ff136a
Paul Tobin <tobster111@gmail.com>
2023-02-25 09:39:54 +0000

    Button

--- button/db/schema.rb
+++ button/db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_22_191427) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "upvote"
+ActiveRecord::Schema[7.0].define(version: 20_230_222_191_427) do
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title'
+    t.text 'body'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'upvote'
   end
-
 end
