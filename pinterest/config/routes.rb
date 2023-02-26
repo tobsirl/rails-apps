be2518c1ca8d92b926804e17faa3ea1915c198f2
Paul Tobin <tobster111@gmail.com>
2023-02-26 11:22:10 +0000

    Pinterest logout button

--- pinterest/config/routes.rb
+++ pinterest/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pinterest/index'
+  # pinterest index controller
+  get 'pinterest/index' => 'pinterest#index'
+  get 'pinterest/auth' => 'pinterest#index'
   root 'pages#home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
