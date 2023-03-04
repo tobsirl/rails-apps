cc9874e2a3747a41798c860a73d031f74eea59ec
Paul Tobin <tobster111@gmail.com>
2023-03-04 10:34:37 +0000

    Devise-api setup

---
+++ devise-api/app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  skip_before_action :verify_authenticity_token, raise: false
+  before_action :authenticate_devise_api_token!, only: [:restricted]
+
+  def home
+  end
+
+  def restricted
+    devise_api_token = current_devise_api_token
+  end
+end
