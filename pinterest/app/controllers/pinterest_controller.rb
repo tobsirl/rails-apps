b34cb53e2737dcb0de1b02a845c01d2af9d60427
Paul Tobin <tobster111@gmail.com>
2023-03-05 11:40:57 +0000

    Pinterest

--- pinterest/app/controllers/pinterest_controller.rb
+++ pinterest/app/controllers/pinterest_controller.rb
@@ -1,6 +1,10 @@
 class PinterestController < ApplicationController
   def index
     client_id = ENV['PINTEREST_CLIENT_ID']
+    redirect_uri = "https://127.0.0.1:3000/oauth/pinterest/oauth_response/"
     puts "client_id: #{client_id}"
+    puts "redirect_uri: #{redirect_uri}"
+    auth_url = "https://www.pinterest.com/oauth/?client_id=#{client_id}&redirect_uri=#{redirect_uri}&response_type=code&scope=boards:read,pins:read"
+    redirect_to auth_url, allow_other_host: true
   end
 end
