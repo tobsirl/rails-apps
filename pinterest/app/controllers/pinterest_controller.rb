cb60f6231d4a383febacace72ff34250d91dff41
Paul Tobin <tobster111@gmail.com>
2023-03-12 10:26:24 +0000

    Pinterest adding state

--- pinterest/app/controllers/pinterest_controller.rb
+++ pinterest/app/controllers/pinterest_controller.rb
@@ -4,7 +4,7 @@ class PinterestController < ApplicationController
     redirect_uri = "http://localhost:3000/oauth/pinterest/oauth_response/"
     puts "client_id: #{client_id}"
     puts "redirect_uri: #{redirect_uri}"
-    auth_url = "https://www.pinterest.com/oauth/?client_id=#{client_id}&redirect_uri=#{redirect_uri}&response_type=code&scope=boards:read,pins:read"
+    auth_url = "https://www.pinterest.com/oauth/?client_id=#{client_id}&redirect_uri=#{redirect_uri}&response_type=code&scope=boards:read,pins:read&state=123"
     redirect_to auth_url, allow_other_host: true
   end
 
@@ -14,6 +14,8 @@ class PinterestController < ApplicationController
     puts "params: #{params}"
     @code = params[:code]
     puts "code: #{@code}"
+    # get state param
+    @state = params[:state]
     client_id = ENV['PINTEREST_CLIENT_ID']
     client_secret = ENV['PINTEREST_CLIENT_SECRET']
   end
