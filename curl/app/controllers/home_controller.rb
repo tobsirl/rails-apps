c15104a9b31fbec109f3aaa6c99c96b739504349
Paul Tobin <tobster111@gmail.com>
2023-04-28 16:05:46 +0100

    Curl

--- curl/app/controllers/home_controller.rb
+++ curl/app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     # render plain: "Hello, Rails!"
-    render plain: "Hello, Rails!"
+    render plain: "Hello, Rails! #{request.params[:foo]}"
   end
 end
