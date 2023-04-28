eb8f023f7ed9d02d70db38d3d34029fd5497feb2
Paul Tobin <tobster111@gmail.com>
2023-04-28 16:09:36 +0100

    Curl

--- curl/app/controllers/home_controller.rb
+++ curl/app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
+  protect_from_forgery with: :null_session
   def index
     # render plain: "Hello, Rails!"
     render plain: "Hello, Rails! #{request.params[:foo]}"
+  end
+
+  def post_example
+    render plain: "You've hit the create action!"
   end
 end
