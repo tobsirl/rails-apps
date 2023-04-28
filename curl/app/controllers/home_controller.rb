a182b42bb94e22e3200c24416c27f8c0bf4e7b23
Paul Tobin <tobster111@gmail.com>
2023-04-28 16:13:09 +0100

    Curl

--- curl/app/controllers/home_controller.rb
+++ curl/app/controllers/home_controller.rb
@@ -1,11 +1,16 @@
 class HomeController < ApplicationController
   protect_from_forgery with: :null_session
+
+  # curl -X GET http://127.0.0.1:3000/index\?foo\=Paul 
+  # get example with query string
   def index
     # render plain: "Hello, Rails!"
     render plain: "Hello, Rails! #{request.params[:foo]}"
   end
 
+  # curl -X POST -d "name=Paul" http://127.0.0.1:3000/post_example
+  # post example with body
   def post_example
-    render plain: "You've hit the create action!"
+    render plain: "You've hit the create action! #{request.body.read}"
   end
 end
